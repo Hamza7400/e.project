41fcd4078d00d80b3dfd7b71f4a581e885eb539c
Hamza Mughal <149361646+Hamza7400@users.noreply.github.com>
2023-10-30 16:59:33 +0500

    add all file

---
+++ assets/js/checkUser.js
@@ -0,0 +1,8 @@
+(() => {
+    var isLogin = sessionStorage.getItem("user");
+    console.log(isLogin)
+    if (isLogin != "true") {
+        location.href = '../../signuplogin.html'
+    }
+  })()
+  
